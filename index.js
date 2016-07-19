4449f3b32abd4b68e19fa7ec0ec2c511e9af52bd
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-19 12:56:11 +0200

    Move selection classes into the state module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {Slice, Fragment} = require("../model")
 const {ios, mac} = require("../util/platform")
 const Keymap = require("browserkeymap")
 const {charCategory, isExtendingChar} = require("../util/char")
-const {Selection, TextSelection, NodeSelection} = require("../selection")
+const {Selection, TextSelection, NodeSelection} = require("../state")
 
 // !! This module exports a number of ‘commands‘, functions that take
 // a ProseMirror instance and try to perform some action on it,
