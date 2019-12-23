358193bb57fb1fe55d093dcf3e654427a089ab92
jongsung <dazzyjong@gmail.com>
2019-12-23 18:53:31 +0900

    clone paragraph actor id

--- src/commands.js
+++ src/commands.js
@@ -322,7 +322,7 @@ export function splitBlock(state, dispatch, view) {
       can = true
     }
     if (can) {
-      tr.split(tr.mapping.map($from.pos), 1, types)
+      tr.split(tr.mapping.map($from.pos), 1, null)
       if (!atEnd && !$from.parentOffset && $from.parent.type != deflt &&
           $from.node(-1).canReplace($from.index(-1), $from.indexAfter(-1), Fragment.from(deflt.create(), $from.parent)))
         tr.setNodeMarkup(tr.mapping.map($from.before()), deflt)
