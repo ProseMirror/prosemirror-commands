dfb852636f9ab353a13789d93ad52524dbcad6b6
jongsung <dazzyjong@gmail.com>
2020-01-10 16:41:21 +0900

    resolve issue which if adding a new line, the style is applied to two lines

--- src/commands.js
+++ src/commands.js
@@ -501,20 +501,8 @@ function getMarkRange($pos = null, type = null) {
     return false
   }
 
-  let startIndex = $pos.index()
   let startPos = $pos.start() + start.offset
-  let endIndex = startIndex + 1
   let endPos = startPos + start.node.nodeSize
-
-  while (startIndex > 0 && link.isInSet($pos.parent.child(startIndex - 1).marks)) {
-    startIndex -= 1
-    startPos -= $pos.parent.child(startIndex).nodeSize
-  }
-
-  while (endIndex < $pos.parent.childCount && link.isInSet($pos.parent.child(endIndex).marks)) {
-    endPos += $pos.parent.child(endIndex).nodeSize
-    endIndex += 1
-  }
 
   return { from: startPos, to: endPos }
 
